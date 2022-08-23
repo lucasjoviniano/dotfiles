b3a478875db412e9792c288223ed6c884b6fcca1
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-08-23 15:43:17 -0300

    Neovim Config

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,211 @@
+require "paq" {
+	"savq/paq-nvim";
+	"franbach/miramare";
+  "nvim-treesitter/nvim-treesitter";
+  "sheerun/vim-polyglot";
+  "nvim-lua/plenary.nvim";
+  {"nvim-telescope/telescope.nvim", branch="0.1.x"};
+  "nvim-lualine/lualine.nvim";
+  "kyazdani42/nvim-web-devicons";
+  "kyazdani42/nvim-tree.lua";
+  "williamboman/mason.nvim";
+  "williamboman/mason-lspconfig.nvim";
+  "neovim/nvim-lspconfig";
+  "simrat39/rust-tools.nvim";
+  "hrsh7th/nvim-cmp"; 
+  "hrsh7th/cmp-nvim-lsp";
+  "hrsh7th/cmp-nvim-lua";
+  "hrsh7th/cmp-nvim-lsp-signature-help";
+  "hrsh7th/cmp-vsnip";                             
+  "hrsh7th/cmp-path";                              
+  "hrsh7th/cmp-buffer";                            
+  "hrsh7th/vim-vsnip";
+  "puremourning/vimspector";
+  "lewis6991/impatient.nvim";
+  "voldikss/vim-floaterm";
+  "nvim-lua/plenary.nvim";
+  "folke/todo-comments.nvim";
+  "windwp/nvim-autopairs";
+  "tpope/vim-surround";
+  -- "jose-elias-alvarez/null-ls.nvim";
+  "tpope/vim-commentary";
+  "wakatime/vim-wakatime";
+}
+
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- require("null-ls").setup({
+--     sources = {
+--         require("null-ls").builtins.formatting.gofmt,
+--     },
+--     -- you can reuse a shared lspconfig on_attach callback here
+--     -- on_attach = function(client, bufnr)
+--     --     if client.supports_method("textDocument/formatting") then
+--     --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--     --         vim.api.nvim_create_autocmd("BufWritePre", {
+--     --             group = augroup,
+--     --             buffer = bufnr,
+--     --             callback = function()
+--     --                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+--     --                 vim.lsp.buf.formatting_sync()
+--     --             end,
+--     --         })
+--     --     end
+--     -- end,
+-- })
+
+require'lspconfig'.gopls.setup{}
+
+require'lspconfig'.elixirls.setup{
+    cmd = { "/home/lucas/lsp/elixir-ls/language_server.sh" };
+}
+
+require("todo-comments").setup {}
+require("nvim-autopairs").setup {}
+
+require("nvim-tree").setup()
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require('lualine').setup()
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml", "go", "elixir" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['tsserver'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
+
+local rt = {
+    server = {
+        settings = {
+            on_attach = function(_, bufnr)
+                -- Hover actions
+                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                -- Code action groups
+                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                require 'illuminate'.on_attach(client)
+            end,
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy"
+                }, 
+            },
+        }
+    },
+}
+require('rust-tools').setup(rt)
+
+-- Completion Plugin Setup
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
+})
---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,110 @@
+local indent, width = 2, 80
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = indent
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = indent
+vim.opt.termguicolors = true
+vim.opt.textwidth = width
+vim.opt.wrap = false
+
+vim.cmd [[colorscheme miramare]]
+vim.cmd [[let mapleader=","]]
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+
+vim.keymap.set("n", '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set("n", '<leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set("n", '<leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set("n", '<leader>fc', '<cmd>Telescope colorscheme<cr>')
+
+vim.keymap.set('n', '<C-p>', '<cmd>NvimTreeToggle<cr>')
+
+vim.keymap.set('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=1 <CR> ")
+vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
+vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>")
+
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Vimspector
+vim.cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+]])
+vim.keymap.set('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
+vim.keymap.set('n', "Dw", ":call vimspector#AddWatch()<cr>")
+vim.keymap.set('n', "De", ":call vimspector#Evaluate()<cr>")
+
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+
+vim.keymap.set('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish <CR> ")
+vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
+vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>")
